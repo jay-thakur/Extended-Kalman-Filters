110a5f7555966cebdfcd41a9ec1beb4edf7434f7
JAY PRAKASH THAKUR <jayprakashthakursnr@gmail.com>
2019-01-12 16:50:46 +0000

    Update : Laser

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -144,7 +144,10 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     ekf_.UpdateEKF(measurement_pack.raw_measurements_);
 
   } else {
-    // TODO: Laser updates
+    // Laser updates
+    ekf_.H_ = H_laser_;
+    ekf_.R_ = R_laser_;
+    ekf_.Update(measurement_pack.raw_measurements_);
 
   }
 
