8a408c69fe895d3b5ec68d4b0aae5c98e5bc3fad
JAY PRAKASH THAKUR <jayprakashthakursnr@gmail.com>
2019-01-12 16:48:18 +0000

    Update : Radar

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -138,7 +138,10 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    */
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
-    // TODO: Radar updates
+    // Radar updates
+    ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
+    ekf_.R_ = R_radar_;
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
 
   } else {
     // TODO: Laser updates
