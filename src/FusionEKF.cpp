ea1653fba6cc0bc7d0bc6d4461148916f1352577
JAY PRAKASH THAKUR <jayprakashthakursnr@gmail.com>
2019-01-12 16:13:37 +0000

    FusionEKF init + set noises

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -36,7 +36,11 @@ FusionEKF::FusionEKF() {
    * TODO: Finish initializing the FusionEKF.
    * TODO: Set the process and measurement noises
    */
-
+  H_laser_ << 1, 0, 0, 0, 
+              0, 1, 0, 0;
+  
+  noise_ax = 9;
+  noise_ay = 9;
 
 }
 
